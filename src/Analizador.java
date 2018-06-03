8b9295a006b540d6ffbf20657c0ce76a34499b32
Lautaro Martin Emanuel <emlautarom1@gmail.com>
2018-06-02 22:27:31 -0300

    Segunda version con un solo superstring con todo el HTML

--- src/Analizador.java
+++ src/Analizador.java
@@ -7,82 +7,38 @@ public class Analizador {
 
     public static void main(String[] args) {
 
-        String superString = "";
-
         List<String> inputFile = new ArrayList<>();
-        inputFile.add("<img src><input type=\\\"__\\\" name=\\\"__\\\" id=\\\"_\\\" pattern=\\\"Expresion");
-        inputFile.add("_regular\\\"> <input >");
+        inputFile.add("<img src><input type=\"__\" name=\"__\" id=\"_\" pattern=\"Expresion");
+        inputFile.add("_no_");
+        inputFile.add("regular\"> <input >");
+        inputFile.add("<head></head>");
 
-        // Lsita de strings de salida del HTML.
-        List<String> outputfile = new ArrayList<>();
-
-        int lineSkipper = 0;
-        Pattern patternTags = Pattern.compile("[<]");
-
-        for (int i = 0; i < inputFile.size(); i++) {
-            if (lineSkipper > 0) {
-                lineSkipper--;
-            } else {
-                String input = inputFile.get(i);
-                System.out.println("Reading string: " + input);
-
-                // Tomamos el string analizado y lo separamos por tags.
-                String[] tagsList = patternTags.split(input);
-
-                for (String tagSection : tagsList) {
-
-                    System.out.println("Actual tag: " + tagSection);
-
-                    // Si el tag extraido es del formato de input entonces resulta de interes para analizar.
-                    if (tagSection.matches("\\s*input\\s+(.)*")) {
-
-                        System.out.println("Section " + tagSection + " has input!");
-                        superString = superString.concat(tagSection);
-                        // Lo concatenamos al superstring del formato input
-
-                        // Si no lo tiene, hay que analizar la siguiente linea hasta encontrarlo
-                        if (tagSection.matches("(.)*>(\\s)*$")) {
-                            // Si el string en cuestion tiene el simbolo de cierre al final, terminar.
-                            System.out.println("Input on single line!");
-                        } else {
-                            System.out.println("Multi line input, cheking next lines:");
-                            for (int j = i+1; j < inputFile.size(); j++) {
-                                lineSkipper++;
-                                superString = superString.concat(inputFile.get(j));
-
-                                System.out.println("Next line, looking for closing symbol: "+ inputFile.get(j));
-                                if (inputFile.get(j).contains(">")) {
-                                    System.out.println("Found closing symbol");
-                                    break;
-                                } else {
-                                    System.out.println("Closing symbol not found, cheking next line");
-                                    lineSkipper++;
-                                }
-                            }
-                        }
-                        // Concatenaria todos los inputs en uno solo si es que hay multiples input por linea.
-                        // Con un break solo seria el primero en la linea
-                        break;
-
-
-                    } else {
-                        System.out.println("Section " + tagSection + " is not an input!");
-                    }
-
-                    System.out.println("--------");
-                }
-            }
-        }
-
+        inputSplitter(fileToHTML(inputFile));
 
     }
 
-    private static void quoteExtractor(String superString) {
+
+    private static boolean inputCheker(String inputLine) {
+        char[] inputArray = inputLine.toCharArray();
+        if (quoteAnalysis(inputLine)) {
+            boolean inQuotes = false;
+            for (char c : inputArray) {
+                if (!inQuotes) {
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean quoteAnalysis(String superString) {
         Pattern p = Pattern.compile("\"([^\"]*)\"");
         Matcher m = p.matcher(superString);
         while (m.find()) {
-            System.out.println(containsIllegals(m.group(1)));
+            if (containsIllegals(m.group(1))) {
+                return false;
+            }
         }
+        return true;
     }
 
     // Verifica que el string pasado por parametro no contenga caracteres invalidos.
@@ -92,18 +48,33 @@ public class Analizador {
         return matcher.find();
     }
 
-    private static void tagSeparator() {
-        String stringToSearch = "<p>Yada yada yada <code>foo</code> yada yada ...\n"
-                + "more here <code>bar</code> etc etc\n"
-                + "and still more <code>baz</code> and now the end</p>\n";
+    private static String fileToHTML(List<String> inputFile) {
+        String superS = "";
+        for (String s : inputFile) {
+            superS = superS.concat(s).concat("\n");
+        }
+        return superS;
+    }
 
-        // the pattern we want to search for
-        Pattern p = Pattern.compile(" <code>(\\w+)</code> ", Pattern.MULTILINE);
-        Matcher m = p.matcher(stringToSearch);
+    private static void inputSplitter(String s) {
+        String preparedS;
+
+        preparedS = s.replace("\n", "");
+        preparedS = preparedS.replace(">", ">\n");
+
+        Pattern p = Pattern.compile("<(\\s*input\\s+.*\\s*)>", Pattern.MULTILINE);
+        Matcher m = p.matcher(preparedS);
 
         // print all the matches that we find
-        while (m.find()) {
+        while (m.find())
+        {
             System.out.println(m.group(1));
+            if ( quoteAnalysis(m.group(1)) ){
+                System.out.println("Valid input format!");
+            }
+            else {
+                System.out.println("Invalid char found!");
+            }
         }
     }
 
@@ -121,49 +92,100 @@ public class Analizador {
         }
     }
 
-    private static List<Error> validacion_formularios(List<Linea> archivo_html) { // implementar
-        List<Error> errores_encontrados = new ArrayList<>();
+    public static void extractorAfanado(){
+        String stringToSearch = "<p>Yada yada yada <code>foo</code> yada yada ...\n"
+                + "more here <code>bar</code> etc etc\n"
+                + "and still more <code>baz</code> and now the end</p>\n";
 
-        char[] caracteres;
-        char[] bloqueA = null;
-        char[] invalidos = {'"', '=', '¿', '¡', ' ',};
-        char[] validos = {};
+        // the pattern we want to search for
+        Pattern p = Pattern.compile(" <code>(\\w+)</code> ", Pattern.MULTILINE);
+        Matcher m = p.matcher(stringToSearch);
 
-        // Deteccion de error
-        for (Linea l : archivo_html) {
-            if (l.get_linea().contains("<input")) {
-                // Convertimos a cadena de caracteres. Verficiar los atributos.
-                caracteres = l.get_linea().toCharArray();
-                for (int i = 0; i < caracteres.length; i++) {
-                    if (caracteres[i] == '=') {
-                        int j = i;
-                        while (caracteres[j] != '"') {
-                            // recorro la linea hasta encontrar el primer igual
-                            j++;
-                        }
-                        while (caracteres[j + 1] != '"') {
-                            // recorro los caracteres que hay entre las comillas
-                            int indice = 0;
-                            // indice para que lo llene desde la posicion 0
-                            bloqueA[indice] = caracteres[j];
-                            // Asingno ese bloque de caracteres a un nuevo bloque para ser analizado.
-                            indice++;
-                            j++;
-                        }
-                        // Analisis del bloqueA
-                        for (i = 0; i < bloqueA.length; i++) {
-                            for (j = 0; j < invalidos.length; j++) {
-                                if (bloqueA[i] == invalidos[j]) { /* se ha detectado un error */
-                                    Error e = new Error(archivo_html.indexOf(l), "Error en input, caracteres no validos");
-                                    errores_encontrados.add(e);
+        // print all the matches that we find
+        while (m.find())
+        {
+            System.out.println(m.group(1));
+        }
+    }
+
+    private static void primeraVersion(List<String> inputFile){
+        String superString = "";
+
+        // Lista de strings de salida del HTML.
+        List<String> outputfile = new ArrayList<>();
+
+        int lineSkipper = 0;
+        Pattern pTagInit = Pattern.compile("[<]");
+
+        for (int i = 0; i < inputFile.size(); i++) {
+            System.out.println("Line value: " + i);
+            System.out.println("Lineskipper value: " + lineSkipper);
+
+            // Iteramos sobre el documento entero.
+            if (lineSkipper == 0) {
+
+                // Recuperamos la linea que estamos viendo
+                String input = inputFile.get(i);
+                System.out.println("Reading string number " + i + ": " + input);
+
+                // Tomamos la linea analizada y la separamos por tags.
+                String[] tagsList = pTagInit.split(input);
+
+                for (String tagSection : tagsList) {
+
+                    // Procedemos a analizar por tags
+                    System.out.println("    Actual tag: " + tagSection);
+
+                    // Si el tag extraido es del formato de input entonces resulta de interes para analizar.
+                    if (tagSection.matches("\\s*input\\s+(.)*")) {
+
+                        // Lo concatenamos al superstring del formato input
+                        System.out.println("    Tag \"" + tagSection + "\" is of input type!");
+                        superString = superString.concat(tagSection);
+
+                        if (tagSection.matches("(.)*>(\\s)*$")) {
+                            // Si el string en cuestion tiene el simbolo de cierre al final, terminar.
+                            System.out.println("    Input on single line!");
+                        } else {
+                            // Si no lo tiene, hay que analizar la siguiente linea hasta encontrarlo
+                            System.out.println("    Multi line input, cheking next lines:");
+                            int j = i + 1;
+                            while (j < inputFile.size()) {
+                                superString = superString.concat(inputFile.get(j));
+
+                                System.out.println("        Looking for closing symbol in next line: " + inputFile.get(j));
+                                if (inputFile.get(j).contains(">")) {
+                                    System.out.println("        Found closing symbol!");
+                                    j = inputFile.size();
+                                } else {
+                                    System.out.println("        Closing symbol not found, cheking next line");
+                                    lineSkipper++;
                                 }
+                                j++;
                             }
                         }
+                        System.out.println("    SuperString input: " + superString);
+                        System.out.println("    Cleaning SuperString input...");
+                        superString = superString.substring(0, superString.indexOf(">"));
+                        System.out.println("    SuperString input cleaned: " + superString);
+                        System.out.println("    Validating input ...");
+                        inputCheker(superString);
+                        System.out.println("    --------");
+                        superString = "";
+
+                        // Concatenaria todos los inputs en uno solo si es que hay multiples input por linea.
+                        // Con un break solo seria el primero en la linea
+                        break;
+                    } else {
+                        System.out.println("    Tag \"" + tagSection + "\" is not an input!");
                     }
-                    // aca habria que controlar lo que va despues del igual y antes del igual.
+                    System.out.println("    --------");
                 }
+            } else {
+                // Si tuvimos que skipear una linea, la ignoramos
+                lineSkipper--;
             }
         }
-        return errores_encontrados;
+        System.out.println("Reached end of file!");
     }
 }
